55068c6f83d3ef33bd4a7858deb13ca551c5af8a
kanlayanee <kanlayanee.t@rmutsvmail.com>
2020-07-15 14:58:12 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
+        System.out.println("kanlayanee Trongkaew");
+        System.out.println("s363211760033");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+        System.out.println("เทคโนโลยีการจัดการ");
+
+
 
 
 

a8d1787ffe1bb8241057c4f5a67235ded9ad12da
kanlayanee <kanlayanee.t@rmutsvmail.com>
2020-07-29 15:32:55 +0700

    BasicInput

---
+++ src/Lab2/BasicInput.java
@@ -0,0 +1,28 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BasicInput {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter you name ?:");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is"+name);
+
+        System.out.print("อายุเท่าไหร่ ?:");
+        int age =scanner.nextInt();
+        System.out.println("อายุของคุณคือ"+age);
+
+        System.out.print("Enter you nick name ?:");
+        String nickname = scanner.nextLine();
+        System.out.println("Your nick name is"+name);
+
+
+
+
+    }//main
+
+}//class

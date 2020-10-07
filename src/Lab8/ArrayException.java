66e9b7e8184c7010a311904b40e8f08d742f3c24
kanlayanee <kanlayanee.t@rmutsvmail.com>
2020-10-07 15:34:03 +0700

    Lab_8 Excaption

---
+++ src/Lab8/ArrayException.java
@@ -0,0 +1,23 @@
+package Lab8;
+
+import java.util.InputMismatchException;
+
+public class ArrayException {
+    public static void main(String[] args) {
+
+        int num[] = {10, 20, 30, 40, 50,}; //index 0-4
+
+        System.out.println(num[0]);
+        try {
+            System.out.println(num[3]);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Index is invalid.");
+            System.out.println(e);
+        }finally {
+            num[4] = 100;
+        }
+
+        System.out.println(num[4]);
+
+    }
+}
